982eb0f7a7867594e5457ece26f46232c17eee2e
josh3021 <joseonghwan3021@gmail.com>
2019-08-19 14:18:52 +0900

    #4 Consuming the Store

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -3,12 +3,15 @@ import AppPresenter from './AppPresenter';
 import Store from 'store'; // env 파일 설정떄문에 가능
 
 class AppContainer extends React.Component {
+  state = {
+    message: "abc!"
+  }
   render () {
     return (
-      <Store.Provider> 
+      <Store.Provider value={this.state}> 
         <AppPresenter />
       </Store.Provider>
-    )
+    );
   }
 }
 
